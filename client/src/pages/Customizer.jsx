47441d05ec7f75fe968473d2cceba3a3fbf3b783
Dulat <dulat.kussainov@gmail.com>
2023-07-14 13:10:49 -0400

    final version

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,13 +60,16 @@ const Customizer = () => {
       //call backend to generate an ai image
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://threejs-prroject-ai.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
